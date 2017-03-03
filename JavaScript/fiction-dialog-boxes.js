fa3e94273a5c7092ceb7c3a0a1551bd709cb8b98
Taylor Herb <tah77@pitt.edu>
2017-03-03 13:42:37 -0500

    Add files via upload

---
+++ JavaScript/fiction-dialog-boxes.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function(){
+  $("#info").click(function(){
+    $("#dialog1").dialog({
+      modal:true
+    });
+  });
+});
+
+
+$(document).ready(function(){
+  $("#info2").click(function(){
+    $("#dialog2").dialog({
+      modal:true
+    });
+  });
+});
+
+
+$(document).ready(function(){
+  $("#info3").click(function(){
+    $("#dialog3").dialog({
+      modal:true
+    });
+  });
+});
+
+
+$(document).ready(function(){
+  $("#info4").click(function(){
+    $("#dialog4").dialog({
+      modal:true
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#info5").click(function(){
+    $("#dialog5").dialog({
+      modal:true
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#info6").click(function(){
+    $("#dialog6").dialog({
+      modal:true
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#info7").click(function(){
+    $("#dialog7").dialog({
+      modal:true
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#info8").click(function(){
+    $("#dialog8").dialog({
+      modal:true
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("#info9").click(function(){
+    $("#dialog9").dialog({
+      modal:true
+    });
+  });
+});
